60d9fdfeca671ffe06aa1643670f69b149c6748f
Kapil <sharmaakapil01@gmail.com>
2023-06-06 16:06:43 +0530

    feat: completed the cart functionality

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -3,7 +3,7 @@ import {Form, Button, Card, Row, Col, ListGroup, Image} from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
-import { addToCart } from '../slices/cartSlice';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
 const CartScreen = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
@@ -11,6 +11,14 @@ const CartScreen = () => {
     const addToCartHandler = async (product,qty)=> {
         dispatch(addToCart({...product,qty}));
     }
+    const removeFromCartHandler = async (id)=>{
+        dispatch(removeFromCart(id));
+    }
+
+    const checkoutHandler = ()=>{
+        navigate('/login?redirect=/shipping')
+    }
+
     const {cartItems} = cart;
   return (
         <Row>
@@ -48,7 +56,7 @@ const CartScreen = () => {
                                     </Form.Control>
                                     </Col>
                                     <Col md={2}>
-                                        <Button type='button' variant='light'>
+                                        <Button type='button' variant='light' onClick = {()=> removeFromCartHandler(item._id)}>
                                         <i class="fa fa-trash" aria-hidden="true"></i>
                                         </Button>
                                     </Col>
@@ -69,7 +77,11 @@ const CartScreen = () => {
                                             ${cartItems.reduce((acc,item)=> acc + item.qty*item.price,0).toFixed(2)}
                                             </ListGroup.Item>
                                             <ListGroup.Item>
-                                                <Button type="button" className="btn-block" disabled={cartItems.length === 0 }>
+                                                <Button 
+                                                type="button" 
+                                                className="btn-block" 
+                                                disabled={cartItems.length === 0 }
+                                                onClick={checkoutHandler}>
                                                     Proceed to Checkout
                                                 </Button>
                                             </ListGroup.Item>
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -19,9 +19,13 @@ const cartSlice = createSlice({
                 state.cartItems = [...state.cartItems, item]
             }
             return updateCart(state)
+        },
+        removeFromCart: (state,action)=>{
+            state.cartItems = state.cartItems.filter((item)=> item._id !== action.payload);
+            return updateCart(state)
         }
     }
 })
 
-export const {addToCart} = cartSlice.actions; 
+export const {addToCart,removeFromCart} = cartSlice.actions; 
 export default cartSlice.reducer;
