31a907bffd634b41191de6fc02d6fef2125db0ce
Kapil <sharmaakapil01@gmail.com>
2023-06-08 18:50:47 +0530

    feat-backend: user resource completed - added authentication, authorization, getUser and updateUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import asyncHandler from "../middleware/asyncHandler.js"
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import generateToken from "../utils/generateToken.js";
 
 dotenv.config();
 // @desc Auth user & get token
@@ -13,16 +14,7 @@ const authUser = asyncHandler(async (req,res)=> {
     
     if(user && (await user.matchPassword(password)))
     {
-        const token = jwt.sign({userId: user._id},process.env.JWT_SECRET,{
-            expiresIn: '30d'
-        });
-
-        res.cookie('jwt',token,{
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000
-        })
+        generateToken(res,user._id)
 
         res.json({
             _id: user._id,
@@ -42,7 +34,36 @@ const authUser = asyncHandler(async (req,res)=> {
 // @route POST /api/users/register
 // @access PUBLIC
 const registerUser = asyncHandler(async (req,res)=>{
-    res.send('register user');
+    const {email,password,name} = req.body;
+    const existingUser = await User.findOne({email});
+    if(existingUser)
+    {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+    
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+
+    if(user)
+    {
+        generateToken(res,user._id);
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin 
+        })
+    }
+    else
+    {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 })
 
 // @desc Logout user / clear cookie
@@ -50,14 +71,33 @@ const registerUser = asyncHandler(async (req,res)=>{
 // @access PRIVATE
 
 const logoutUser = asyncHandler(async (req,res)=> {
-    res.send('logout user');
+    res.cookie('jwt','', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+
+    res.status(200).json({message: 'Logged out successfully'});
 })
 
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Public
 const getUserProfile = asyncHandler(async (req,res)=> {
-    res.send('get user profile')
+    const user = await User.findById(req.user._id);
+
+    if(user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    }
+    else
+    {
+        res.status(404);
+        throw new Error('User not found');
+    }
 })
 
 // @desc Update user profile
@@ -65,7 +105,29 @@ const getUserProfile = asyncHandler(async (req,res)=> {
 // @access Private
 
 const updateUserProfile = asyncHandler(async (req,res)=> {
-    res.send('update user profile')
+    const user = await User.findById(req.user._id);
+
+    if(user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+    }
+    else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 })
 
 // @desc  Get users
